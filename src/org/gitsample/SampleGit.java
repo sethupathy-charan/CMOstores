e1f9b77ecc2c4a59dcc873248ba22e68d4953ccd
sethupathy-charan <sethupathycharan@gmail.com>
2021-07-13 18:55:26 +0530

    master

--- src/org/gitsample/SampleGit.java
+++ src/org/gitsample/SampleGit.java
@@ -5,7 +5,7 @@ public class SampleGit {
 	public static void main(String[] args) {
 		System.out.println("Master");
 		System.out.println("Charan");
-		System.out.println("Ajay");
+		System.out.println("Ajays");
 	}
 
 }
